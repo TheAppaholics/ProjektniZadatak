45c149f90eefbac26270dd6ee58c62456e271eec
TheAppaholics <appaholicst@gmail.com>
2025-07-12 09:54:52 +0200

    zavrsena app

--- TeamRegistrationApi/TeamRegistrationApi/obj/Debug/net8.0/TeamRegistrationApi.AssemblyInfo.cs
+++ TeamRegistrationApi/TeamRegistrationApi/obj/Debug/net8.0/TeamRegistrationApi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TeamRegistrationApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8b23f686abf10c6d3d311245795f66dd3c9caa8c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a6d964ec92ccb2253cd1c50610bb86159ef93bf7")]
 [assembly: System.Reflection.AssemblyProductAttribute("TeamRegistrationApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TeamRegistrationApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
